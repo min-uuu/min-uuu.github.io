e5f4669cc9c697dd83b794124aef9bd3668ad426
min-uuu <minuuu03@gmail.com>
2022-12-06 17:52:37 +0900

    리뉴얼 예고 업데이트

--- landing/landing.js
+++ landing/landing.js
@@ -1,22 +1,25 @@
-const text = document.getElementById("introduce-amin")
+alert(
+    "2022년 고등학교 졸업을 앞두고 리뉴얼을 진행하고 있습니다, 조금만 기다려주세요!"
+);
+window.location.replace("https://portfolio.minyou.us");
+// const text = document.getElementById("introduce-amin")
 
-text.innerHTML = '준비된 능력자'
+// text.innerHTML = '준비된 능력자'
 
-var items = [
-    '서비스 기획자', '프로젝트 매니저', '친화력을 가진', '새로움을 가진', '리더쉽을 가진', '커뮤니케이터', '배려심을 가진', '아이디어를 가진', '관찰력을 가진', '댕댕미를 가진', '미래를 만드는', '추진력을 가진', '사람을 만드는', '준비된 능력자'
-];
+// var items = [
+//     '서비스 기획자', '프로젝트 매니저', '친화력을 가진', '새로움을 가진', '리더쉽을 가진', '커뮤니케이터', '배려심을 가진', '아이디어를 가진', '관찰력을 가진', '댕댕미를 가진', '미래를 만드는', '추진력을 가진', '사람을 만드는', '준비된 능력자'
+// ];
 
-var answer
-var number = 0
+// var answer
+// var number = 0
 
-function print() {
-    answer = items[number];
-    text.innerHTML = answer;
-    number = number + 1;
-    if (number === items.length) {
-        number = 0
-    }
-}
+// function print() {
+//     answer = items[number];
+//     text.innerHTML = answer;
+//     number = number + 1;
+//     if (number === items.length) {
+//         number = 0
+//     }
+// }
 
-setInterval(print, 1800)
-
+// setInterval(print, 1800)
